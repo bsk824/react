9d40e61a6dac0f849024ca9e530cece6c1ef098f
플래티어/백승기-디지털채널플랫폼개발팀/김범수 <H2205413@hkmc.hkmg.global>
2022-06-07 17:32:32 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Guide from 'components/guide/guide'
+import 'assets/scss/common.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <h1><Link to="/">APP</Link></h1>
+        <nav>
+          <Link to="/guide">guide</Link>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/guide" element={<Guide />}/>
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
