6fbbf768ea91946268e04b3d407ad10707d81702
플래티어/백승기-디지털채널플랫폼개발팀/김범수 <H2205413@hkmc.hkmg.global>
2022-06-28 16:57:58 +0900

    수정

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,136 +1,163 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Calendar = () => {
-
+  const [selectValue, setSelectValue] = useState(null);
+  const [caleDays, setDays] = useState([]);
   const prevShowNum = 0;
   const nextShowNum = 0;
-  let selectedDate = '20220430';
   
   const returnStr = (number) => {
     if(number < 10) number = '0' + number;
     return number;
   }
 
-  const returnDateStr = (str) => {
-    const year = str.substring(0, 4);
-    const month = returnStr(Number(str.substring(4, 6)) + 1);
-    const day = str.substring(6, 8);
-    return `${year}${month}${day}`;
-  }
-  
-  selectedDate = returnDateStr('20220430');
+  const render = () => {
+    let dayArry = [];
+    const date = new Date();
+    const utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
+    const kstGap = 9 * 60 * 60 * 1000;
+    const now = new Date(utc + kstGap);
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const todayStr = `${now.getFullYear()}${returnStr(now.getMonth() + 1)}${returnStr(now.getDate())}`;
+    
+    let currentYear = today.getFullYear();
+    let currentMonth = today.getMonth();
+    let currentDay = null;
 
-  let dayArry = [];
-
-  const date = new Date();
-  const utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
-  const kstGap = 9 * 60 * 60 * 1000;
-  const now = new Date(utc + kstGap);
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const todayStr = `${now.getFullYear()}${returnStr(now.getMonth())}${returnStr(now.getDate())}`;
-  
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth();
-
-  const startDay = new Date(currentYear, currentMonth, 0);
-  const prevDay = startDay.getDay();
-  const prevDate = startDay.getDate();
-  
-  let showPrevDay = prevDate - prevShowNum - prevDay;
-  
-  for (let prevDayNum = showPrevDay; prevDayNum <= prevDate; prevDayNum++) {
-    let prevYear = currentYear;
-    let prevMonth = currentMonth - 1;
-    if(prevMonth < 0) {
-      prevMonth = 11;
-      prevYear = currentYear - 1;
+    if(selectValue != null) {
+      const selectYear = selectValue.substring(0, 4);
+      const selectMonth = returnStr(Number(selectValue.substring(4, 6)) - 1);
+      const selectDay = returnStr(selectValue.substring(6, 8));
+      const selectDate = new Date(selectYear, selectMonth, selectDay);
+      currentYear = selectDate.getFullYear();
+      currentMonth = selectDate.getMonth();
+      currentDay = selectDate.getDay();
     }
-    prevMonth = returnStr(prevMonth);
-    prevDayNum = returnStr(prevDayNum);
-    let date = new Date(prevYear, prevMonth, prevDayNum);
-    let dateStr =  `${prevYear}${prevMonth}${prevDayNum}`;
 
-    dayArry.push(
-      {
-        date: date,
-        day: prevDayNum,
-        prev: true,
-        today : (todayStr === dateStr) ? true : false,
-        selected : (selectedDate === dateStr) ? true : false,
+    const startDay = new Date(currentYear, currentMonth, 0);
+    const prevDay = startDay.getDay();
+    const prevDate = startDay.getDate();
+    
+    let showPrevDay = prevDate - prevShowNum - prevDay;
+    
+    for (let prevDayNum = showPrevDay; prevDayNum <= prevDate; prevDayNum++) {
+      let prevYear = currentYear;
+      let prevMonth = currentMonth - 1;
+      if(prevMonth < 0) {
+        prevMonth = 11;
+        prevYear = currentYear - 1;
       }
-    )
-  }
+      prevMonth = returnStr(prevMonth + 1);
+      prevDayNum = returnStr(prevDayNum);
+      let dateStr =  `${prevYear}${prevMonth}${prevDayNum}`;
 
-  const endDay = new Date(currentYear, currentMonth + 1, 0);
-  
-  const nextDate = endDay.getDate();
-
-  for (let days = 1; days <= nextDate; days++) {
-    let year = currentYear;
-    let month = currentMonth;
-    month = returnStr(month);
-    days = returnStr(days);
-    let date = new Date(year, month, days);
-    let dateStr =  `${year}${month}${days}`;
-
-    dayArry.push(
-      {
-        date: date,
-        day: days,
-        today : (todayStr === dateStr) ? true : false,
-        selected : (selectedDate === dateStr) ? true : false,
-      }
-    )
-  }
-
-  const nextDay = endDay.getDay();
-
-  let showNextDay = (6 - nextDay === 6) ? 0 : 6 - nextDay;
-  showNextDay = showNextDay + nextShowNum;
-
-  for (let nextDayNum = 1; nextDayNum <= showNextDay; nextDayNum++) {
-    let nextYear = currentYear;
-    let nextMonth = currentMonth + 1;
-    if(nextMonth > 11) {
-      nextMonth = 0;
-      nextYear = currentYear + 1;
+      dayArry.push(
+        {
+          date: dateStr,
+          day: prevDayNum,
+          prev: true,
+          today : (todayStr === dateStr) ? true : false,
+          select : (selectValue === dateStr) ? true : false,
+        }
+      )
     }
-    nextMonth = returnStr(nextMonth);
-    nextDayNum = returnStr(nextDayNum);
-    let date = new Date(nextYear, nextMonth, nextDayNum);
-    let dateStr =  `${nextYear}${nextMonth}${nextDayNum}`;
 
-    dayArry.push(
-      {
-        date: date,
-        day: nextDayNum,
-        next: true,
-        today : (todayStr === dateStr) ? true : false,
-        selected : (selectedDate === dateStr) ? true : false,
+    const endDay = new Date(currentYear, currentMonth + 1, 0);
+    
+    const nextDate = endDay.getDate();
+
+    for (let days = 1; days <= nextDate; days++) {
+      let year = currentYear;
+      let month = currentMonth;
+      month = returnStr(month + 1);
+      days = returnStr(days);
+      let dateStr = `${year}${month}${days}`;
+
+      dayArry.push(
+        {
+          date: dateStr,
+          day: days,
+          today : (todayStr === dateStr) ? true : false,
+          select : (selectValue === dateStr) ? true : false,
+        }
+      )
+    }
+
+    const nextDay = endDay.getDay();
+
+    
+    console.log(6 - nextDay);
+    
+    let showNextDay = (6 - nextDay === 6) ? 6 : 6 - nextDay;
+    showNextDay = (showNextDay === 0) ? 7 : showNextDay;
+    showNextDay = showNextDay + nextShowNum;
+
+    for (let nextDayNum = 1; nextDayNum <= showNextDay; nextDayNum++) {
+      let nextYear = currentYear;
+      let nextMonth = currentMonth + 1;
+      if(nextMonth > 11) {
+        nextMonth = 0;
+        nextYear = currentYear + 1;
       }
-    )
+      nextMonth = returnStr(nextMonth + 1);
+      nextDayNum = returnStr(nextDayNum);
+      let dateStr =  `${nextYear}${nextMonth}${nextDayNum}`;
+
+      dayArry.push(
+        {
+          date: dateStr,
+          day: nextDayNum,
+          next: true,
+          today : (todayStr === dateStr) ? true : false,
+          select : (selectValue === dateStr) ? true : false,
+        }
+      )
+    }
+
+    return dayArry;
   }
 
-  const returnDate = (arg) => {
-    console.log(arg);
+	useEffect(()=>{
+    setDays(render());
+  }, [selectValue]);
+
+  const selectDate = (arg) => {
+    setSelectValue(arg);
   }
-  
+
+  const setRender = (e) => {
+    if(e.key === 'Enter') {
+      setSelectValue(e.target.value);
+    }
+  }
+
   return(
     <>
-      {dayArry.map(date => (
-        <div
-          key={date.date}
-          onClick={()=>returnDate(date.date)}
-          className={
-            (date.today ? 'today ' : '')+
-            (date.prev ? 'prev ': '')+
-            (date.next ? 'next ': '')+
-            (date.selected ? 'selected ': '')
-          }
-        >
-          {date.day}
-        </div>
-			))}
+      <input type="text" onKeyPress={setRender} />
+      <div className='calendar'>
+        <div>일</div>
+        <div>월</div>
+        <div>화</div>
+        <div>수</div>
+        <div>목</div>
+        <div>금</div>
+        <div>토</div>
+        {caleDays.map(date => (
+          <div
+            key={date.date}
+            onClick={()=>selectDate(date.date)}
+            className={
+              (date.today ? 'today ' : '')+
+              (date.prev ? 'prev ': '')+
+              (date.next ? 'next ': '')+
+              (date.select ? 'select ': '')+
+              (date.selected ? 'selected ': '')
+            }
+          >
+            {date.day}
+          </div>
+        ))}
+      </div>
     </>
   ) 
 }
